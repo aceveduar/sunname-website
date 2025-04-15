098788241276dd26835be49476478072f14e58ad
Eduardo Acevedo <aceveduar@MacBook-Air-de-MARIA.local>
2025-04-15 00:23:34 -0600

    Sección del ParthnerShip ok

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const boton = document.getElementById('miBoton');
-    
-    boton.addEventListener('click', () => {
-        alert('¡Hola! Has hecho clic en el botón');
-    });
-}); 
